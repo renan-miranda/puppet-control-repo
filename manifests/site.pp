113d35b824b8953caa6c75eb93e9a9120db5e657
renan-miranda <renan.f.de.miranda@gmail.com>
2019-02-26 17:02:14 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'master.puppet.cloud' {
   include role::master_server
   file {'/root/README':
     ensure  => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}\n",
   }
 }
 node /ˆweb/ {
